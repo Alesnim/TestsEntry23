f1e35fd9e722ff8d675253741ec7d44fd4cf4463
Alexey Kapanov <alesnim@gmail.com>
2021-11-07 16:46:40 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // тест-кейсы
+        sumPositive(0, 1);
+        sumPositive(10, 10);
+        sumPositive(-42, 1);
+
+
+    }
+
+
+    // реализуйте метод сложения положительных чисел
+    // для включения assertов служит опция -ea
+    public static int sumPositive(int a, int b) {
+        return a+b;
+
+    }
+
+
+    // реализуйте метод поиска элементов в массиве
+
+
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    private int arOrig[];
+
+
+    @BeforeEach
+    void setUp() {
+        Random r = new Random();
+        arOrig = r.ints(1_000_000, 0, 1000).toArray();
+    }
+
+
+    @Test
+    void sumPositive(){
+        assertEquals(Main.sumPositive(1,1), 2);
+    }
+
+
+
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of(1, 1),
+                Arguments.of(2, 2),
+                Arguments.of(5, 3),
+                Arguments.of(6, 6)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    void sumPositiveParametrize(int a, int b) {
+
+        assertEquals(a + b, Main.sumPositive(a, b));
+    }
+
+
+
+}
